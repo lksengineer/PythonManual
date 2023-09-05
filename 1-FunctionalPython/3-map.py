5fcb9129913552923257c1e38b77e3bbecc35cee
lksengineer <ksaavedrasu@gmail.com>
2023-09-05 23:54:04 +0000

    feature/FunctionalProgramming/FunctionProgramming-Lambda-Map

--- 1-FunctionalPython/3-map.py
+++ 1-FunctionalPython/3-map.py
@@ -8,8 +8,26 @@ Si la función f requiere n argumentos entonces deben pasarse n colecciones con 
 Para convertir el objeto en una lista, tupla o diccionario hay que aplicar explícitamente las funciones list(), tuple() o dic() respectivamente.
 """
 
+# Mode 1
 def square(n):
-  return n*n
+  return n * n
 
-result = list(map(func, [1, 2, 3]))
+result = list(map(square, [1, 2, 3]))
 print(result)
+
+# Mode 2 with lambda
+square = list(map(lambda n: n * n, [1, 2, 3]))
+print(square)
+
+
+# Mode 1: Rectangle with two(2) iterables
+def rectangle(x, y):
+  return x * y
+  
+rentangle = tuple(map(rectangle, (1, 2, 3), (2, 3, 4)))
+print(rentangle)
+
+
+# Mode 2: Rectangle with two(2) iterables and lambda
+rentangle = tuple(map(lambda x, y: x * y, (1, 2, 3), (2, 3, 4)))
+print(rentangle)
